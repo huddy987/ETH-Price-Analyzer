fb1b20b72f18f5678af06b61317ce1f9f7dc9ab1
Hudson S <huddy987@gmail.com>
2019-07-13 18:27:43 -0600

    Make setup easier
    - Add requirements.txt
    - Add dummy_keys.py

---
+++ dummy_keys.py
@@ -0,0 +1,64 @@
+# Binance
+# https://github.com/binance-exchange/binance-official-api-docs/blob/master/rest-api.md
+BINANCE_USDT_WALLET = 0
+BINANCE_ETH_WALLET = 0
+BINANCE_API_KEY = 0
+BINANCE_SECRET_KEY = 0
+
+# Bittrex
+# https://bittrex.github.io/api/v1-1
+BITTREX_USDT_WALLET = 0
+BITTREX_ETH_WALLET = 0
+BITTREX_API_KEY = 0
+BITTREX_SECRET_KEY = 0
+
+# OKEx
+# https://www.okex.com/docs/en/#README
+OKEX_USDT_WALLET = 0
+OKEX_ETH_WALLET = 0
+OKEX_API_KEY = 0
+OKEX_SECRET_KEY = 0
+
+
+# HUOBI
+# https://HUOBIapi.github.io/docs/v1/en/#access-urls
+HUOBI_USDT_WALLET = 0
+HUOBI_ETH_WALLET = 0
+HUOBI_API_KEY = 0
+HUOBI_SECRET_KEY = 0
+
+# Poloniex
+# https://docs.poloniex.com/#introduction
+POLONIEX_USDT_WALLET = 0
+POLONIEX_ETH_WALLET = 0
+POLONIEX_API_KEY = 0
+POLONIEX_SECRET_KEY = 0
+
+
+# Bitforex
+# https://github.com/bitforexapi
+BITFOREX_USDT_WALLET = 0
+BITFOREX_ETH_WALLET = 0
+BITFOREX_API_KEY = 0
+BITFOREX_SECRET_KEY = 0
+
+# KuCoin
+# https://docs.kucoin.com/
+KUCOIN_USDT_WALLET = 0
+KUCOIN_ETH_WALLET = 0
+KUCOIN_API_KEY = 0
+KUCOIN_SECRET_KEY = 0
+
+# Bitfinex
+# https://docs.bitfinex.com/docs
+BITFINEX_USDT_WALLET = 0
+BITFINEX_ETH_WALLET = 0
+BITFINEX_API_KEY = 0
+BITFINEX_SECRET_KEY = 0
+
+# Trade.io
+# https://github.com/tradeio/exchange-api-doc/blob/master/rest-api.md
+TRADEIO_USDT_WALLET = 0
+TRADEIO_ETH_WALLET = 0
+TRADEIO_API_KEY = 0
+TRADEIO_SECRET_KEY = 0
