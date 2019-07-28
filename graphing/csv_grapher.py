c81be8b68ea5f2b9fc9938ba68b72fe2289a8140
huddy987 <huddy987@gmail.com>
2019-07-27 18:56:26 -0600

    Add figure legend

--- graphing/csv_grapher.py
+++ graphing/csv_grapher.py
@@ -36,14 +36,32 @@ def generate_data(filename, row):
         line = line.split(",")
         data.append(float(line[row]))
 
+# Converts epoch to a timestamp usable by pyplot
 def convert_epoch(timestamps):
     return_array = list()
     for timestamp in timestamps:
         return_array.append(timestamp/1000)
     return md.epoch2num(return_array)
 
+# Gets the exchange names from the header
+def get_exchange_names(filename):
+    file = open(filename, "r+")
+    exchanges = list()
+
+    # Read the header
+    line = file.readline()
+    line = line.split(",")
+
+    for i in range(exchange_number):
+        name_cell = line[i+2] # Exchanges start at cell 3
+        name_cell = name_cell.split()
+        exchanges.append(name_cell[0])
+
+    return exchanges
+
+
 # Plots the data
-def plot(x, y_array):
+def plot(x, y_array, exchange_names):
     fig, ax1 = plt.subplots()
 
     # Date/time formatting
@@ -57,13 +75,15 @@ def plot(x, y_array):
     ax1.set_ylabel("Price (USD)")
 
     # Print all of the y data
-    for y in y_array:
-        ax1.plot(x, y)
+    for i,y in enumerate(y_array):
+        ax1.plot(x, y, label=exchange_names[i])
 
     # Set all of the colors
     colors = ["Red", "Green", "Blue", "Pink", "Purple", "Orange", "Brown", "Grey", "Yellow", "Black"]
     for i,j in enumerate(ax1.lines):
         j.set_color(colors[i])
+
+    plt.legend(loc='lower left')
 
     plt.show()
 
@@ -74,13 +94,17 @@ if __name__ == "__main__":
 
     # Get the time data
     date_time = generate_data(filename, 0)
+
     # Convert epoch timestamps to proper format for pyplot
     date_time = convert_epoch(date_time)
 
     # Get the price data
     price_array = list()
     for i in range(exchange_number):
-        price_array.append(generate_data(filename, (i + 2))) # Start at the second cell
+        price_array.append(generate_data(filename, (i + 2))) # Start at the 3rd cell
+
+    # Get exchange names in the same order as the prices
+    exchange_names = get_exchange_names(filename)
 
     # Plot the data
-    plot(date_time, price_array)
+    plot(date_time, price_array, exchange_names)
